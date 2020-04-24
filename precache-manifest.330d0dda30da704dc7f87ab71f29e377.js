3b3c4c9d5ca5c9fd0e4f0799d2820310939d8de7
Svetlana Vasilkova <rusowass@mail.ru>
2020-04-24 19:50:59 +0300

    Updates

---
+++ precache-manifest.330d0dda30da704dc7f87ab71f29e377.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e720b25519f9cdf38de4a9479ac57e4a",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "bc2785faf062a85b81ea",
+    "url": "/weather-forecast/static/js/2.8a7f6f8c.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/weather-forecast/static/js/2.8a7f6f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2837d27bd228260543",
+    "url": "/weather-forecast/static/js/main.f61f2f8a.chunk.js"
+  },
+  {
+    "revision": "12e2e9674e799cc37853",
+    "url": "/weather-forecast/static/js/runtime-main.7302c0f4.js"
+  },
+  {
+    "revision": "a69e78d500107fb2dcbb943436a22f85",
+    "url": "/weather-forecast/static/media/cloud-dark.a69e78d5.svg"
+  },
+  {
+    "revision": "a67651839b63d1132346768082d14110",
+    "url": "/weather-forecast/static/media/humidity.a6765183.svg"
+  },
+  {
+    "revision": "0c48e29de96fed68c8ac5189ef56a50b",
+    "url": "/weather-forecast/static/media/loupe.0c48e29d.svg"
+  },
+  {
+    "revision": "e61177a55b03974430e9c32b24029377",
+    "url": "/weather-forecast/static/media/pressure.e61177a5.svg"
+  },
+  {
+    "revision": "d9b2d03dc90505701265782df1138739",
+    "url": "/weather-forecast/static/media/sky.d9b2d03d.png"
+  },
+  {
+    "revision": "090e58b6b963249a096f23a1b8ad2261",
+    "url": "/weather-forecast/static/media/sunny-light.090e58b6.svg"
+  },
+  {
+    "revision": "330e5376c8144c0e7910df3441e45cc0",
+    "url": "/weather-forecast/static/media/winter.330e5376.svg"
+  }
+]);
